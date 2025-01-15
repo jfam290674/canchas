43018c1cefb12ddc9c9ca7012a25e18b34a38c8e
jfam290674 <jfavila@nexosecuador.site>
2025-01-15 11:21:51 -0500

    cambio 15 02

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Cambiar a 'react-dom/client' para React 18
 
 const App = () => {
     return <h1 className="text-3xl font-bold underline">¡Hola, React + Tailwind!</h1>;
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Seleccionar el contenedor raíz
+const rootElement = document.getElementById('root');
+
+// Crear el root para React 18
+if (rootElement) {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(<App />);
+}
